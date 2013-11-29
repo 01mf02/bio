d461417e83da5be7ced5affe81c8e8cafa072ad3
Michael Färber <01mf02@gmail.com>
2013-11-29 18:23:00 +0100

    Implement weight vector neighbors.

--- ass5.ml
+++ ass5.ml
@@ -1,19 +1,51 @@
 open List;;
 
-(* TODO *)
-let distance v1 v2 = 0;;
+type point = float list;;
+type weight = float list;;
+type population = point array;;
+type weights = weight list;;
+
+
+(* List/vector helper functions *)
+
+let rec take n = function
+  [] -> []
+| hd::tl -> if n > 0 then hd::(take (n-1) tl) else [];;
+
+(* taken from OCaml 4.00 *)
+let rec mapi i f = function
+| [] -> []
+| a::l -> let r = f i a in r :: mapi (i + 1) f l;;
+let mapi f l = mapi 0 f l;;
+
+let lsum  = fold_left ( +. ) 0.0;;
+let lprod = fold_left ( *. ) 1.0;;
+
+let vsum  v1 v2 = map (fun (x, y) -> x +. y) (combine v1 v2);;
+let vdiff v1 v2 = map (fun (x, y) -> x -. y) (combine v1 v2);;
+let vdotprod v1 v2 =
+  fold_left (fun a (x1, x2) -> a +. (x1 *. x2)) 0.0 (combine v1 v2);;
+
+let vdist v1 v2 =
+  let d = vdiff v1 v2 in sqrt (vdotprod d d);;
+
+
+(* MOEA/D *)
+
+let weight_neighbors (ws : weights) t =
+  map (fun w1 ->
+    let dists = mapi (fun i2 w2 -> (vdist w1 w2, i2)) ws in
+    map snd (take t (sort compare dists))) ws;;
+
 
 let rec moead ep population = function
 | 0 -> ep
-| n -> moead ep population (n-1)
+| n ->
+    moead ep population (n-1)
 
 let _ =
   (* weight vector *)
   let wv = [] in
-  (* weight vector distances *)
-  let wvd = [] in
-  (* weight vector neighbors *)
-  let wvn = [] in
 
   let initial_population = [] in
 
