962def2e8262a1f20583360ee4548c3a20bc58b4
Michael Färber <01mf02@gmail.com>
2013-12-08 04:50:03 +0100

    Fix some bugs and extend algorithm.

--- ass5.ml
+++ ass5.ml
@@ -16,6 +16,10 @@ let rec random_float_list min max = function
   0 -> []
 | n -> (Random.float (max -. min) +. min) :: (random_float_list min max (n-1));;
 
+let print_float_list = iter (Printf.printf "%f ");;
+let print_float_list_list =
+  iter (fun l -> print_float_list l; print_newline());;
+
 let rec range_excl i j = if i >= j then [] else i :: (range_excl (i+1) j);;
 let rec range_incl i j = if i >  j then [] else i :: (range_incl (i+1) j);;
 
@@ -40,18 +44,23 @@ let vdotprod v1 v2 =
 let vdist v1 v2 =
   let d = vdiff v1 v2 in sqrt (vdotprod d d);;
 
+let between limit_min limit_max v = min limit_max (max limit_min v);;
+
 
 (* Optimization functions *)
 
 let calculate_j1 n = filter (fun x -> x mod 2 = 1) (range_incl 2 n);;
 let calculate_j2 n = filter (fun x -> x mod 2 = 0) (range_incl 2 n);;
 
+let pareto_set j x1 nf =
+  let jf = float_of_int j in
+  let fraction = (3.0 *. (jf-.2.0) /. (nf-.2.0)) in
+  let exponent = 0.5 *. (1.0 +. fraction) in
+  x1**exponent;;
+
 let f12sum jn x x1 nf = lsum (map (fun j ->
-	let jf = float_of_int j in
-	let xj = nth x j in
-	let fraction = (3.0 *. (jf-.2.0) /. (nf-.2.0)) in
-	let exponent = 0.5 *. (1.0 +. fraction) in
-	(xj -. x1**exponent)**2.0) jn);;
+  let xj = nth x (j-1) in
+  (xj -. pareto_set j x1 nf)**2.0) jn);;
 
 let f12common x =
   let n  = length x in
@@ -77,7 +86,7 @@ let rec generate_solutions min max dim = function
 | n -> (random_float_list min max dim) :: generate_solutions min max dim (n-1);;
 
 let calculate_weight_vectors2 n =
-  let l = range_incl 0 n in
+  let l = range_excl 0 n in
   let nf = float_of_int n in
   map (fun x -> let xf = float_of_int x in [xf /. nf; (nf -. xf) /. nf]) l;;
 
@@ -91,16 +100,26 @@ let dominates v1 v2 =
   for_all (fun (x1, x2) -> x1 >= x2) c &&
   exists  (fun (x1, x2) -> x1 >  x2) c;;
 
-let reproduce s1 s2 = s1;;
+let recombine s1 s2 = map (fun (x1, x2) -> (x1 +. x2) /. 2.0) (combine s1 s2);;
+
+let mutate v div = map (fun x ->
+  if Random.int (length v) = 0 then
+    between 0.0 1.0 (x +. (Random.float (div *. 2.0)) -. div)
+  else x) v;;
 
 (* weighted sum approach *)
 let g_ws x ws fs = lsum (map (fun (w, f) -> w *. (f x)) (combine ws fs));;
 
 let update solutions neighbors weights (*bests*) fs ep =
   (* reproduction *)
-  let k = random_from_list neighbors in
-  let l = random_from_list neighbors in
-  let y = reproduce (nth solutions k) (nth solutions l) in
+  (* usually only recombine solutions from within the neighborhood, but
+   * on average every 10th time, recombine a solution from neighborhood with
+   * a solution from anywhere *)
+  let k =
+    nth solutions (random_from_list neighbors) in
+  let l = if Random.int 10 = 0 then random_from_list solutions else
+    nth solutions (random_from_list neighbors) in
+  let y = mutate (recombine k l) 0.2 in
   
   (* update bests *)
   (*let bests' = map (fun (b, f) -> max b (f y)) (combine bests fs) in*)
@@ -117,13 +136,9 @@ let update solutions neighbors weights (*bests*) fs ep =
   (solutions', ep'');;
 
 let rec update_all solutions_init neighbors weights fs ep_init =
-  let rec f i solutions ep =
-    if i >= length solutions then (solutions, ep)
-    else
-      let (solutions', ep') = update solutions (nth neighbors i) weights fs ep in
-      f (succ i) solutions' ep' in
-
-  f 0 solutions_init ep_init;;
+  fold_left (fun (solutions, ep) neighbor ->
+    update solutions neighbor weights fs ep)
+  (solutions_init, ep_init) neighbors;;
 
 
 let rec moead solutions neighbors weights fs ep = function
@@ -134,8 +149,8 @@ let rec moead solutions neighbors weights fs ep = function
 
 let _ =
   let solutions_n = 300 in
-  let neighborhood_size = 2 in
-  let iterations = 100 in
+  let neighborhood_size = 3 in
+  let iterations = 10 in
   let vec_dimension = 10 in
 
   let weights = calculate_weight_vectors2 solutions_n in
@@ -146,5 +161,7 @@ let _ =
 
   let ep = moead initial_solutions neighbors weights fs [] iterations in
 
-  Printf.printf "Hello world!\n";
+  print_endline "EP:";
+  print_float_list_list ep;
+  Printf.printf "Number of elements in EP: %d\n" (length ep);
 ;
